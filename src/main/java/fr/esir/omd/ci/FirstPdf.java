ddccc761e5fe7691bc40979441a4676fcbcee694
Sterenn <slh35@hotmail.fr>
2023-11-10 12:52:05 +0100

    Suppression d'un if

--- src/main/java/fr/esir/omd/ci/FirstPdf.java
+++ src/main/java/fr/esir/omd/ci/FirstPdf.java
@@ -62,8 +62,6 @@ public class FirstPdf {
     int i = 0;
     if (i == 0){
       int j = 2;
-    } else if (i == 1) {
-      int j = 3;
     } else if (i == 2) {
       int j = 3;
     } else if (i == 3) {
